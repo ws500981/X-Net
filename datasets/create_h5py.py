331d6d5261f248e37d2f617c2b35e8b768196e34
MSwML <51591899+MSwML@users.noreply.github.com>
2020-02-25 15:37:19 +0800

    minor

--- datasets/create_h5py.py
+++ datasets/create_h5py.py
@@ -28,7 +28,7 @@ def save2h5py(ds_pth, h5py_pth):
     # 1) add all label volumes tgr 
     # 2) save label to array  
     # 3) save lesion to array 
-    ds_pth = ds_pth + '/*/c*/c*'
+    ds_pth = ds_pth + '/standard_part*/c*/c*'
     listofpaths = glob.glob(ds_pth, recursive=True) #get folder paths (each folder is for one brain with lesion+labels)
     assert len(listofpaths) > 0, 'No directory found that follows the defined pattern: %s. The glob results are: %s'%(ds_pth, str(listofpaths))
     listofpaths.sort()
