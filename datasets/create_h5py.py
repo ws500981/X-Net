532731ca1c31db32241b8689c85f9e7dd6e5d43f
MSwML <51591899+MSwML@users.noreply.github.com>
2020-02-25 12:15:42 +0800

    minor

--- datasets/create_h5py.py
+++ datasets/create_h5py.py
@@ -169,7 +169,8 @@ def normalise(array, new_min, new_max):
     old_min = float(array.min())
     array = (new_max - new_min) * (array - old_min) / (old_max - old_min) + new_min
 
-    assert array.max() == new_max and array.min() == new_min
+    assert array.max() == new_max , 'Normalization fails: requested new_max=%.3f, results new_max=%.3f'%(new_max, array.max())
+    assert array.min() == new_min , 'Normalization fails: requested new_min=%.3f, results new_min=%.3f'%(new_min, array.min())
     
     return array
 
